4d60576604107b126bb3be68bea4258868f26704
tanuj <tanujts20@gmail.com>
2025-12-28 14:59:06 +0530

    feat: auth init

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,5 +13,8 @@ const prisma = new PrismaClient({
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
-    })
+    }),
+    emailAndPassword: {
+        enabled: true
+    }
 })
