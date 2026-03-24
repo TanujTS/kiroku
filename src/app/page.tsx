4150cd5cdb72adb17181cda0ff6b99d0f9b9cbd0
Tanuj <tanujts20@gmail.com>
2026-03-24 09:28:29 +0530

    feat: init color theme and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-const backgroundStyle = {
-  background: `
-    radial-gradient(circle at top right, rgba(173, 201, 130, 0.08), transparent 40%),
-    radial-gradient(circle at bottom left, rgba(173, 201, 130, 0.05), transparent 40%),
-    #0a090c
-  `
-};
 
 export default function Home() {
   return (
-    <div style={backgroundStyle} className="w-screen h-screen flex items-center justify-center">
-
-    </div>
+    <>
+      <Button className="font-heading"> Hi </Button>
+    </>
   );
 }
